8291abae86639a341fbb3aac15b9443f23ba71dc
Alex Karmazyn <ak650@cam.ac.uk>
2013-03-11 19:33:16 +0000

    Correction to fov subsection of cloud. Embedded position from cloud and rigid body alignment but neither prove effective

--- accumulator/nodes/accumulator.py
+++ accumulator/nodes/accumulator.py
@@ -34,7 +34,9 @@ import cv2
 import tf
 import time
 import std_msgs.msg
+import message_filters
 from geometry_msgs.msg import Point32
+from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Polygon
 from geometry_msgs.msg import PolygonStamped
 from sensor_msgs.msg import PointCloud
@@ -80,6 +82,8 @@ class Accumulator:
         self.desc = None # Mxdesc_length
         self.first_time = True
         self.count = 0
+        self.R = np.diag((1.,1.,1.))
+        self.t = np.zeros((3,1))
         
     def connect(self):
         print 'Waiting for calibration ... '
@@ -89,12 +93,18 @@ class Accumulator:
         self.cloud_pub = rospy.Publisher('/accumulator/absolute_cloud',PointCloud)
         self.desc_pub = rospy.Publisher('/accumulator/absolute_described_cloud', Described3DPoints)
         self.fov_pub = rospy.Publisher('/accumulator/fov', PolygonStamped)
+        self.testa_pub = rospy.Publisher('/cloud_compare/test_a',PointCloud)
+        self.testb_pub = rospy.Publisher('/cloud_compare/test_b',PointCloud)
+        self.testc_pub = rospy.Publisher('/cloud_compare/test_c', PointCloud)
         
-    def on_got_cloud(self, cloud):
+    def on_got_cloud(self, cloud):        
         self.stamp = cloud.header.stamp
         self.time_prev = time.time()
         prevtime = time.time()
         #print "Received new cloud"
+        """
+        # Decode Cloud
+        """
         self.decode_and_buffer_message(cloud)
         new = self.frames[-1]
         #print "Decoded ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
@@ -121,23 +131,27 @@ class Accumulator:
             self.publish_cloud()
             return
         
+        """
         # Get subset of points that lie infront of new pose
+        """
         # i.e. points that could have been seen
         kp_masked, desc_masked, shifts = self.get_subset_fov(new.position_i, new.quat_i_to_w, new.quat_w_to_i)
         #print "Sectioned ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
         prevtime = time.time()
         
-        print kp_masked.shape
-        print desc_masked.shape
+        
         
         original_length = self.mean.shape[1]
-        
+        """
         # Match points
+        """
         kp1, kp2, indices1, indices2 = self.match_points(self.frames[-1].kp, kp_masked, new.desc, desc_masked)
         #print "Matched ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
         prevtime = time.time()
         
-        # Reconstruct indices for full self.acc, self.desc
+        """
+        # Reconstruct indices for full self.acc, self.desc (are currently for fov subset)
+        """
         for i in range(len(indices2)):
             indices2[i] = shifts[indices2[i]]
         #print "Rebuilt ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
@@ -145,55 +159,82 @@ class Accumulator:
         
         print "No of matches: ", len(indices2)
         
-        # Get inliers with RANSAC fitted F
-        #indices1F, indices2F = self.extract_fundamental_and_filter_inliers(kp1, kp2, indices1, indices2)
-        #print "Fundamentalled ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
-        #prevtime = time.time()
         
-        #print "No of F fits: ", len(indices1F)
-        
-        
-        
-        #if len(indices1F) > 8:
-        #    R, t = self.approx_register(self.mean[:,indices2F], new_pts[:,indices1F])
-        #    print R
-        #    print t
-        #    new_pts = np.add(new_pts, -t)
-        #    new_pts = R.T.dot(new_pts)
-        
-        # Get inliers with RANSAC fitted F
-        #indices1F, indices2F = self.extract_fundamental_and_filter_inliers(kp1, kp2, indices1, indices2)
-        #print "Fundamentalled ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
-        #prevtime = time.time()
-        #print len(indices1F)
-        
-        mask = np.ones(new_pts.shape[1])
-        Ns = np.zeros(len(indices2), dtype=np.float32)
-        merged_count = 0
-        for i in range(len(indices1)):
-            # Mark point as averaged and update count
-            mask[indices1[i]] = 0
-            merged_count = merged_count + 1
-            Ns[i] = self.acc[indices2[i]].pts.shape[1]
-            # Append new info
-            self.acc[indices2[i]].pts = np.hstack((self.acc[indices2[i]].pts, new_pts[:,indices1[i]:indices1[i]+1]))
-            self.acc[indices2[i]].pts2 = np.hstack((self.acc[indices2[i]].pts2, np.square(new_pts[:,indices1[i]:indices1[i]+1])))
-            '''# E(i)[X2] = Var(i)[X] + (E(i)[X])^2
-            self.var[:,indices2[i]:indices2[i]+1] = self.var[:,indices2[i]:indices2[i]+1] + np.square(self.mean[:,indices2[i]:indices2[i]+1])
+        if len(indices2 > 0):
+            """
+            # Get inliers with RANSAC fitted F
+            """
+            indices1F, indices2F = self.extract_fundamental_and_filter_inliers(kp1, kp2, indices1, indices2)
+            #print "Fundamentalled ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
+            #prevtime = time.time()
+            
+            print "No of F fits: ", len(indices1F)
+            
+            # Attempt to position from existing points
+            self.position_from_cloud(self.mean[:,indices2], kp1, new.position_i)
+            
+            '''
+            """
+            # Rigid body alignment (trans only for now)
+            """
+            if len(indices1F) > 8:
+                R, t = self.very_approx_register(self.mean[:,indices2F], new_pts[:,indices1F])
+                print R
+                print t
+                new_pts = np.add(new_pts, -t)
+                new_pts = R.T.dot(new_pts)
+                self.R = R
+                self.t = t
+                
+                cloud = PointCloud()
+                cloud.header.stamp = cloud.header.stamp
+                cloud.header.frame_id = "/world"
+            
+                # Build relative cloud
+                for i in range(new_pts.shape[1]):
+                    cloud.points.append(Point32())
+                    cloud.points[-1].x = new_pts[0,i]
+                    cloud.points[-1].y = new_pts[1,i]
+                    cloud.points[-1].z = new_pts[2,i]
+                self.testa_pub.publish(cloud)
+            else:
+                new_pts = np.add(new_pts, -self.t)
+                new_pts = self.R.T.dot(new_pts)
+            '''
+            
+            # Get inliers with RANSAC fitted F
+            #indices1F, indices2F = self.extract_fundamental_and_filter_inliers(kp1, kp2, indices1, indices2)
+            #print "Fundamentalled ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
+            #prevtime = time.time()
+            #print len(indices1F)
+            
+            mask = np.ones(new_pts.shape[1])
+            Ns = np.zeros(len(indices2), dtype=np.float32)
+            merged_count = 0
+            for i in range(len(indices1)):
+                # Mark point as averaged and update count
+                mask[indices1[i]] = 0
+                merged_count = merged_count + 1
+                Ns[i] = self.acc[indices2[i]].pts.shape[1]
+                # Append new info
+                self.acc[indices2[i]].pts = np.hstack((self.acc[indices2[i]].pts, new_pts[:,indices1[i]:indices1[i]+1]))
+                self.acc[indices2[i]].pts2 = np.hstack((self.acc[indices2[i]].pts2, np.square(new_pts[:,indices1[i]:indices1[i]+1])))
+                '''# E(i)[X2] = Var(i)[X] + (E(i)[X])^2
+                self.var[:,indices2[i]:indices2[i]+1] = self.var[:,indices2[i]:indices2[i]+1] + np.square(self.mean[:,indices2[i]:indices2[i]+1])
+                # E(i+1)[X] = (E(i)[X]*(N-1) + xnew)/N
+                self.mean[:,indices2[i]:indices2[i]+1] = (self.mean[:,indices2[i]:indices2[i]+1]*(self.acc[indices2[i]].pts.shape[1]-1.)+self.acc[indices2[i]].pts[:,-1:])/float(self.acc[indices2[i]].pts.shape[1])
+                # Var(i+1)[X] = (E(i)[X2]*(N-1)+x2new)/N - (E(i+1)[X])^2
+                self.var[:,indices2[i]:indices2[i]+1]  = (self.var[:,indices2[i]:indices2[i]+1]*(self.acc[indices2[i]].pts.shape[1]-1.)+self.acc[indices2[i]].pts2[:,-1:])/float(self.acc[indices2[i]].pts.shape[1]) - np.square(self.mean[:,indices2[i]:indices2[i]+1])'''
+            # Efficiently iteratively update all means and vars
+            # E(i)[X2] = Var(i)[X] + (E(i)[X])^2
+            self.var[:,indices2] = self.var[:,indices2] + np.square(self.mean[:,indices2])
             # E(i+1)[X] = (E(i)[X]*(N-1) + xnew)/N
-            self.mean[:,indices2[i]:indices2[i]+1] = (self.mean[:,indices2[i]:indices2[i]+1]*(self.acc[indices2[i]].pts.shape[1]-1.)+self.acc[indices2[i]].pts[:,-1:])/float(self.acc[indices2[i]].pts.shape[1])
+            self.mean[:,indices2] = (self.mean[:,indices2]*(Ns-1.)+new_pts[:,indices1])/Ns
             # Var(i+1)[X] = (E(i)[X2]*(N-1)+x2new)/N - (E(i+1)[X])^2
-            self.var[:,indices2[i]:indices2[i]+1]  = (self.var[:,indices2[i]:indices2[i]+1]*(self.acc[indices2[i]].pts.shape[1]-1.)+self.acc[indices2[i]].pts2[:,-1:])/float(self.acc[indices2[i]].pts.shape[1]) - np.square(self.mean[:,indices2[i]:indices2[i]+1])'''
-        # Efficiently iteratively update all means and vars
-        # E(i)[X2] = Var(i)[X] + (E(i)[X])^2
-        self.var[:,indices2] = self.var[:,indices2] + np.square(self.mean[:,indices2])
-        # E(i+1)[X] = (E(i)[X]*(N-1) + xnew)/N
-        self.mean[:,indices2] = (self.mean[:,indices2]*(Ns-1.)+new_pts[:,indices1])/Ns
-        # Var(i+1)[X] = (E(i)[X2]*(N-1)+x2new)/N - (E(i+1)[X])^2
-        self.var[:,indices2]  = self.var[:,indices2]*(Ns-1.)+np.square(new_pts[:,indices1])/Ns - np.square(self.mean[:,indices2])
-        
-        #print "Averaged ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
-        prevtime = time.time()
+            self.var[:,indices2]  = self.var[:,indices2]*(Ns-1.)+np.square(new_pts[:,indices1])/Ns - np.square(self.mean[:,indices2])
+            
+            #print "Averaged ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
+            prevtime = time.time()
         
         # Pre-allocate new mean and var array by adding ones for new cols
         self.mean = np.hstack((self.mean, np.ones((3,new_pts.shape[1]-merged_count))))
@@ -220,6 +261,51 @@ class Accumulator:
         #print "Re-projected ( ", np.around(((time.time()-prevtime)*1000),2), "ms) "
         print "Point Cloud Accumulated ( ", np.around(((time.time()-self.time_prev)*1000),1), "ms) - (",self.mean.shape[1]," unique points out of ", self.count," )"
         
+    def position_from_cloud(self, pts3D_matched, kp_matched, position_i):
+
+        
+        """================================================================
+        # Calculate pose and translation to matched template
+        ================================================================"""
+        
+        if len(self.kp) < 5:
+            "Not enough matches to localise"
+            return
+        
+        print pts3D_matched.shape
+        print kp_matched.shape
+        
+        R, t, inliers = cv2.solvePnPRansac(np.array(pts3D_matched, dtype=np.float32).T, np.array(kp_matched, dtype=np.float32), self.cameraMatrix, self.distCoeffs, reprojectionError=15.)
+        
+        
+        if inliers == None:
+            print "===================="
+            print "Template not found"
+            print "===================="
+            return
+        
+        
+        print "No. of inliers: ", len(inliers)   
+        if len(inliers) > 16: 
+            
+            
+            R, J = cv2.Rodrigues(R)        
+            Rhomo = np.diag((1., 1., 1., 1.))
+            Rhomo[:3, :3] = R
+            
+            t_position = -R.T.dot(t)
+            print "t: ", t_position
+            print "i: ", position_i
+        
+            
+  
+            header = sfwi.header
+            
+            # Publish tf
+            quat = tf.transformations.quaternion_inverse(tf.transformations.quaternion_from_matrix(Rhomo))
+            self.position_i = t_position
+            self.quat_w_to_i = quat 
+        
     def get_subset(self, position_i, quat_i_to_w, quat_w_to_i):
         camera_relative = np.add(tf.transformations.quaternion_matrix(quat_w_to_i)[:3,:3].dot(self.mean), -position_i)
         mask = camera_relative[2] > 0
@@ -229,12 +315,10 @@ class Accumulator:
         
     def get_subset_fov(self, position_i, quat_i_to_w, quat_w_to_i):
         camera_relative = np.add(tf.transformations.quaternion_matrix(quat_w_to_i)[:3,:3].dot(self.mean), -position_i)
-        normed_z = camera_relative/camera_relative[2]
+        camera_relative = camera_relative/camera_relative[2]
         
         bottom_left =  self.inverseCameraMatrix.dot(np.array([[-80.],[405.],[ 1.]]))
-        print "bl: ", bottom_left
         top_right =  self.inverseCameraMatrix.dot(np.array([[720.],[-45.],[1.]]))
-        print "tr: ", top_right
         
         offset = tf.transformations.quaternion_matrix(quat_i_to_w)[:3,:3].dot(position_i)
         spoly = PolygonStamped()
@@ -327,7 +411,14 @@ class Accumulator:
         R = Vt.T.dot(U.T)
         # Recover t that match least squares R
         t = mean2 - R.dot(mean1)
-        print np.hstack((R,t))
+        return R, t
+        
+    def very_approx_register(self, pts1, pts2):
+        # De-couple translation by subtracting centrioids
+        delta = np.add(-pts1, pts2)
+        t = np.array([np.mean(delta, axis=1)]).T
+        print t.shape
+        R = np.diag((1.,1.,1.))
         return R, t
         
     
@@ -389,6 +480,32 @@ class Accumulator:
         i2_pts = kp2[i2_indices,:]
         
         return i1_pts, i2_pts, np.array(i1_indices), np.array(i2_indices)
+        
+    def match_2D3D_points(self, kp, pts3D, desc1, desc2):
+        """Matches the specified point with already triangulated points"""
+        # Match features        
+        matches = self.dm.match(desc1, desc2)
+        matches2 = self.dm.match(desc2, desc1)
+
+        # Produce ordered arrays of paired points
+        i1_indices = list(x.queryIdx for x in matches)
+        i2_indices = list(x.trainIdx for x in matches)
+        i2_indices2 = list(x.queryIdx for x in matches2) 
+        i1_indices2 = list(x.trainIdx for x in matches2)
+        
+        # Find pairing that are consistent in both dirs
+        comb1 = set(zip(i1_indices, i2_indices))
+        comb2 = set(zip(i1_indices2, i2_indices2))
+        comb = list(comb1.intersection(comb2))
+        comb = zip(*list(comb))
+        i1_indices = comb[0]
+        i2_indices = comb[1]
+        
+        # Order pairs
+        kp_matched = kp[i1_indices,:]
+        pts3D_matched = pts3D[:, i2_indices]
+        
+        return kp_matched, pts3D_matched
         
     def extract_fundamental_and_filter_inliers(self, i1_pts_undistorted, i2_pts_undistorted, indices1, indices2):
         """Extract fundamental matrix and then remove outliers"""        
